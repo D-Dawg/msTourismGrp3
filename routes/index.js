4930fbffd53192647e18197c80344821977726cf
d-dawg <dennis.sommer.86@gmail.com>
2017-10-12 19:32:16 +0200

    new version

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/hans', function(req, res, next) {
+router.post('/hans', function(req, res, next) {
   res.json({hans:"hans"});
   // res.render('index', { title: 'Expreshanss' });
 });
