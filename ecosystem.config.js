c252b0313faa706c321e943b862a437e96195db9
d-dawg <dennis.sommer.86@gmail.com>
2017-10-14 21:25:06 +0200

    exammple

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
         // First application
         {
-            name: 'API',
+            name: 'msTourism',
             script: './app.js',
             env: {
                 COMMON_VARIABLE: 'true'
