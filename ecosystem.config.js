f1feda0c209e7f1314830baa6d579cd8a8fd0724
d-dawg <dennis.sommer.86@gmail.com>
2017-10-15 02:15:31 +0200

    old

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
             key: '~/.ssh/id_rsa',
             user: 'dworkz',
             host: '185.216.32.138',
-            ref: 'origin/dep',
+            ref: 'origin/deployment',
             repo: 'git@github.com:D-Dawg/msTourismGrp3.git',
             path: '/home/dworkz/home/msTourism'
             // 'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
